689ff5d12b26d78622e1d9531eb541c5472d19d9
Hacı <haciyakuptetik@gmail.com>
2018-03-25 23:54:08 +0300

    deneme commit

--- Main.py
+++ Main.py
@@ -1,3 +1,8 @@
-list[1,2,3,4]
-for i in list:
-    print(i)
+from github import Github
+
+# using username and password
+g = Github("haciyakup", "Koc1234.")
+
+print(g.get_user())
+# or using an access token
+#g = Github("access_token")
