87771f05893a69ddbaa2f214d52e526a50415b09
Hacı <haciyakuptetik@gmail.com>
2018-03-26 18:06:43 +0300

    repos check in

--- Main.py
+++ Main.py
@@ -6,3 +6,7 @@ g = Github("haciyakup", "Koc1234.")
 print(g.get_user())
 # or using an access token
 #g = Github("access_token")
+
+for repo in g.get_user().get_repos():
+    print(repo.name)
+    repo.edit(has_wiki=False)
