c0abc4e89999c08f7c430a387e2b4dd997fb1158
Hacı <haciyakuptetik@gmail.com>
2018-03-27 08:51:23 +0300

    get commits

--- Main.py
+++ Main.py
@@ -1,12 +1,54 @@
-from github import Github
+#! /usr/bin/env python2
+import json
+import requests
 
-# using username and password
-g = Github("haciyakup", "Koc1234.")
 
-print(g.get_user())
-# or using an access token
-#g = Github("access_token")
+def count_user_commits(user):
+    r = requests.get('https://api.github.com/users/%s/repos' % user)
+    repos = json.loads(r.content)
 
-for repo in g.get_user().get_repos():
-    print(repo.name)
-    repo.edit(has_wiki=False)
+    for repo in repos:
+        if repo['fork'] is True:
+            # skip it
+            continue
+        n = count_repo_commits(repo['url'] + '/commits')
+        repo['num_commits'] = n
+        yield repo
+
+
+def count_repo_commits(commits_url, _acc=0):
+    r = requests.get(commits_url)
+    commits = json.loads(r.content)
+    n = len(commits)
+    if n == 0:
+        return _acc
+    link = r.headers.get('link')
+    if link is None:
+        return _acc + n
+    next_url = find_next(r.headers['link'])
+    if next_url is None:
+        return _acc + n
+    # try to be tail recursive, even when it doesn't matter in CPython
+    return count_repo_commits(next_url, _acc + n)
+
+
+# given a link header from github, find the link for the next url which they use for pagination
+def find_next(link):
+    for l in link.split(','):
+        a, b = l.split(';')
+        if b.strip() == 'rel="next"':
+            return a.strip()[1:-1]
+
+
+if __name__ == '__main__':
+    import sys
+    try:
+        user = 'zozkalkan'
+    except IndexError:
+        print("Usage: %s <username>" % sys.argv[0])
+        sys.exit(1)
+    total = 0
+    for repo in count_user_commits(user):
+        print ("Repo `%(name)s` has %(num_commits)d commits, size %(size)d." % repo)
+        total += repo['num_commits']
+    print ("Total commits: %d" % total)
