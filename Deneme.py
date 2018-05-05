2d568df9cf1b07e9b2713b6ee627d5b3eeb0e777
Hacı <haci.tetik@tiga.com.tr>
2018-05-05 13:34:37 +0300

    download repos tt

--- Deneme.py
+++ Deneme.py
@@ -8,6 +8,6 @@ API_URL = "https://api.github.com/users/haciyakup/repos"
 url = "{}?access_token={}".format(API_URL, API_TOKEN)
 req_json = requests.get(url).json()
 
-
+tt
 for repo in req_json:
     os.system("git clone {}".format(repo["git_url"]))
