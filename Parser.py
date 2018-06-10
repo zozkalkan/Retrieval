5c6c7bdb86520529145e408db503d244fb8a1448
Hacı <haci.tetik@tiga.com.tr>
2018-06-10 13:00:15 +0300

    change main.py

--- Parser.py
+++ Parser.py
@@ -8,6 +8,7 @@ zipName= 'server-develop.zip'
 rtrnValArr=[]
 rtrnVal_=''
 sayac = 0
+
 def Insert_Method_Invocation(class_name, project_name, method_name, variable_name, called_method_name, method_code,path):
     qry = '''INSERT INTO METHOD_INVOCATION
              (CLASS_NAME, PROJECT_NAME,METHOD_NAME,VARIABLE_NAME,CALLED_METHOD_NAME,METHOD_CODE,PATH,CC,LOC)
@@ -75,7 +76,7 @@ def mergeMethodNames(rtrnValArr1,bodyName,default=0):
     rtrnValArr1.append(bodyName)
     mName = ""
     for val in rtrnValArr1:
-        mName = mName + "--->" + val + "()";
+        mName = "{} ---> {}()".format(mName,val)
     return mName
 
 def returnHandler(body,cName,file_path,pMethodName):
@@ -112,58 +113,75 @@ def variableHandler(body,cName,file_path):
     methodVariables = body.variable_declarators
     for params in methodVariables:
         rtrnValArr = []
-
-        if (params.initializer.target == None):
-            rtrnValArr.append(params.initializer.name)
-
-        try:
-            trgt = params.initializer.target
-
-            if hasattr(params.initializer, 'expression'):
-                trgt = params.initializer.expression.target
-
-            while hasattr(trgt, 'name'):
-                if (hasattr(trgt, 'name')):
-                    rtrnValArr.append(trgt.name)
-                    if hasattr(trgt, 'target'):
-                        trgt = trgt.target
-                    else:
-                        rtrnValArr.append(trgt.name.value)
-                        break
-            if (hasattr(trgt, 'value')):
-                rtrnValArr.append(trgt.value)
-
-        except Exception as e:
-            print(e, file_path)
-        # Recursive_Find_target(params.initializer.target)
-        # print(rtrnValArr)
-        if (len(rtrnValArr) == 0):
-            rtrnValArr.append('ownMethod')
-        objectName = rtrnValArr[-1]
-
-        initializerName = ""
-
-        if hasattr(params.initializer, 'expression'):
-            initializerName = params.initializer.expression.name
-        if hasattr(params.initializer, 'name'):
-            initializerName = params.initializer.name
-
-        DBMethodName = mergeMethodNames(rtrnValArr, initializerName)
-
         parentMethodName = "-"
 
         if hasattr(MethodDec, 'name'):
             parentMethodName = MethodDec.name
 
-        Insert_Method_Invocation(cName, 'baz.java', parentMethodName,
-                                 objectName,
-                                 DBMethodName, sayac, file_path)
+        if hasattr(params.initializer,'target'):
+            if (params.initializer.target == None):
+                rtrnValArr.append(params.initializer.name)
 
-        Insert_Variable_Invocation(cName, 'baz.java', parentMethodName,
-                                   params.variable.name,
-                                   body.type.name.value,
-                                   sayac, file_path)
+            try:
+                trgt = params.initializer.target
 
+                if hasattr(params.initializer, 'expression'):
+                    if hasattr(params.initializer.expression ,'target'):
+                        trgt = params.initializer.expression.target
+
+                while hasattr(trgt, 'name'):
+                    if (hasattr(trgt, 'name')):
+                        rtrnValArr.append(trgt.name)
+                        if hasattr(trgt, 'target'):
+                            trgt = trgt.target
+                        else:
+                            rtrnValArr.append(trgt.name.value)
+                            break
+                if (hasattr(trgt, 'value')):
+                    rtrnValArr.append(trgt.value)
+
+            except Exception as e:
+                print(e, file_path)
+            # Recursive_Find_target(params.initializer.target)
+            # print(rtrnValArr)
+            if (len(rtrnValArr) == 0):
+                rtrnValArr.append('ownMethod')
+            objectName = rtrnValArr[-1]
+
+            initializerName = ""
+
+            if hasattr(params.initializer, 'expression'):
+                if hasattr(params.initializer.expression,'name'):
+                    initializerName = params.initializer.expression.name
+                else:
+                    initializerName= params.initializer.expression.value
+            if hasattr(params.initializer, 'name'):
+                initializerName = params.initializer.name
+
+            DBMethodName = mergeMethodNames(rtrnValArr, initializerName)
+
+
+
+            Insert_Method_Invocation(cName, 'baz.java', parentMethodName,
+                                     objectName,
+                                     DBMethodName, sayac, file_path)
+            objectTypes =''
+            if hasattr(body.type,'name'):
+                objectTypes=body.type.name.value
+            else:
+                objectTypes = body.type
+            Insert_Variable_Invocation(cName, 'baz.java', parentMethodName,
+                                       params.variable.name,
+                                       objectTypes,
+                                       sayac, file_path)
+
+        if hasattr(params.initializer,'type'):
+            if hasattr(params.initializer.type,'name'):
+                if hasattr(params.initializer.type.name,'value'):
+                    Insert_Variable_Invocation(cName, 'baz.java', parentMethodName,
+                                       params.variable.name,
+                                           params.initializer.type.name.value,
+                                       sayac, file_path)
 def methodHandler  (body,cName,file_path):
        if hasattr(body, 'name'):
                         rtrnValArr = []
@@ -193,7 +211,6 @@ def methodHandler  (body,cName,file_path):
                         Insert_Method_Invocation(cName, 'baz.java', MethodDec.name,
                                                  objectName,
                                                  DBMethodName, sayac, file_path)
-
 
 def ifElseHandler(body,cName,file_path,pMethodName):
     statements =[]
@@ -226,54 +243,71 @@ def ifElseHandler(body,cName,file_path,pMethodName):
     if hasattr(body.predicate, 'target'):
         methodHandler(body.predicate, cName, file_path)
 
+def forHandler(body,cName,file_path,pMethodName):
+
+    statements= body.statements
+    for state in statements:
+        if hasattr(state, 'block'):
+            tryBlockHandler(state, cName, file_path, pMethodName)
+        if hasattr(state, 'if_true'):
+            ifElseHandler(state, cName, file_path, pMethodName)
+        if hasattr(state, 'result'):
+            returnHandler(state, cName, file_path, pMethodName)
+        if hasattr(state, 'variable_declarators'):
+            variableHandler(state, cName, file_path)
+        if hasattr(state, 'name'):
+            methodHandler(state, cName, file_path)
 
 def tryBlockHandler(body,cName,file_path,pMethodName):
+
     if hasattr(body,'variable_declarators'):
-        methodVariables = body.variable_declarators
-        for params in methodVariables:
-            rtrnValArr = []
+        variableHandler(body, cName, file_path)
+        #methodVariables = body.variable_declarators
+        #for params in methodVariables:
+        #    rtrnValArr = []
 
-            if (params.initializer.target == None):
-                rtrnValArr.append("ownMethod")
+        #    if (params.initializer.target == None):
+        #        rtrnValArr.append("ownMethod")
 
-            try:
-                trgt = params.initializer.target
+        #    try:
+        #        trgt = params.initializer.target
 
-                while hasattr(trgt, 'name'):
-                    if (hasattr(trgt, 'name')):
-                        rtrnValArr.append(trgt.name)
-                        trgt = trgt.target
-                if (hasattr(trgt, 'value')):
-                    rtrnValArr.append(trgt.value)
+        #        while hasattr(trgt, 'name'):
+        #            if (hasattr(trgt, 'name')):
+        #                rtrnValArr.append(trgt.name)
+        #                trgt = trgt.target
+        #        if (hasattr(trgt, 'value')):
+        #            rtrnValArr.append(trgt.value)
 
-            except Exception as e:
-                print(e,file_path)
+
+        #    except Exception as e:
+        #        print(e,file_path)
             # Recursive_Find_target(params.initializer.target)
 
-            if (len(rtrnValArr) == 0):
-                rtrnValArr.append('ownMethod')
-            objectName = rtrnValArr[-1]
-            DBMethodName = mergeMethodNames(rtrnValArr, params.initializer.name)
-            parentMethodName = "-"
+        #    if (len(rtrnValArr) == 0):
+        #        rtrnValArr.append('ownMethod')
+        #    objectName = rtrnValArr[-1]
+        #    DBMethodName = mergeMethodNames(rtrnValArr, params.initializer.name)
+        #    parentMethodName = "-"
 
-            if hasattr(MethodDec, 'name'):
-                parentMethodName = MethodDec.name
+        #    if hasattr(MethodDec, 'name'):
+        #        parentMethodName = MethodDec.name
 
-            Insert_Method_Invocation(cName, 'baz.java', parentMethodName,
-                                     objectName,
-                                     DBMethodName, sayac, file_path)
+        #    Insert_Method_Invocation(cName, 'baz.java', parentMethodName,
+        #                             objectName,
+        #                             DBMethodName, sayac, file_path)
 
-            Insert_Variable_Invocation(cName, 'baz.java', parentMethodName,
-                                       params.variable.name,
-                                       body.type.name.value,
-                                       sayac, file_path)
+        #    Insert_Variable_Invocation(cName, 'baz.java', parentMethodName,
+        #                               params.variable.name,
+        #                               body.type.name.value,
+        #                               sayac, file_path)
 
 
-            if hasattr(body, 'block'):
-                if hasattr(body.block, 'statements'):
-                    declarations = body.block.statements
-                    for dec in declarations:
-                        transactions(dec, cName, file_path)
+    if hasattr(body, 'block'):
+             if hasattr(body.block, 'statements'):
+                declarations = body.block.statements
+                for dec in declarations:
+                    transactions(dec, cName, file_path,sayac)
 
     if hasattr(body,'name'):
             if hasattr(body, 'name'):
@@ -315,84 +349,93 @@ def transactions(MethodDec,cName,file_path,sayac):
     try:
         if hasattr(MethodDec, 'name'):
 
-            methodParams = MethodDec.parameters
+            if hasattr(MethodDec,'parameters'):
+                methodParams = MethodDec.parameters
 
-            for params in methodParams:
-                objectType = ''
+                for params in methodParams:
+                    objectType = ''
 
-                if hasattr(params.type, 'name'):
-                    objectType = params.type.name.value
-                else:
-                    objectType = params.type
+                    if hasattr(params.type, 'name'):
+                        objectType = params.type.name.value
+                    else:
+                        objectType = params.type
 
-                Insert_Variable_Invocation(cName, 'baz.java', MethodDec.name, params.variable.name,
-                                           objectType, sayac, file_path)
+                    Insert_Variable_Invocation(cName, 'baz.java', MethodDec.name, params.variable.name,
+                                               objectType, sayac, file_path)
 
             # method invacation
             if hasattr(MethodDec, 'body'):
                 methodBody = MethodDec.body
-                for body in methodBody:
+                if (methodBody !=None):
+                    for body in methodBody:
 
-                    if hasattr(body, 'name'):
-                        rtrnValArr = []
-                        try:
-                            trgt= body.target
+                        if hasattr(body, 'name'):
+                            rtrnValArr = []
+                            try:
+                                trgt= body.target
 
-                            while hasattr(trgt,'name'):
-                                if (hasattr(trgt, 'name')):
-                                    rtrnValArr.append(trgt.name)
-                                    trgt= trgt.target
-                            if hasattr(trgt,'value'):
-                                rtrnValArr.append(trgt.value)
+                                while hasattr(trgt,'name'):
+                                    if (hasattr(trgt, 'name')):
+                                        rtrnValArr.append(trgt.name)
+                                        trgt= trgt.target
+                                if hasattr(trgt,'value'):
+                                    rtrnValArr.append(trgt.value)
 
-                        except Exception as e:
-                            print(e, file_path)
+                            except Exception as e:
+                                print(e, file_path)
 
-                        #Recursive_Find_target(body.target)
-                        #print(rtrnValArr)
+                            #Recursive_Find_target(body.target)
+                            #print(rtrnValArr)
 
-                        if (len(rtrnValArr) == 0):
-                            rtrnValArr.append('ownMethod')
+                            if (len(rtrnValArr) == 0):
+                                rtrnValArr.append('ownMethod')
 
-                        objectName = rtrnValArr[-1]
+                            objectName = rtrnValArr[-1]
 
-                        DBMethodName = mergeMethodNames(rtrnValArr, body.name)
+                            DBMethodName = mergeMethodNames(rtrnValArr, body.name)
 
-                        Insert_Method_Invocation(cName, 'baz.java', MethodDec.name,
-                                                 objectName,
-                                                 DBMethodName, sayac, file_path)
+                            Insert_Method_Invocation(cName, 'baz.java', MethodDec.name,
+                                                     objectName,
+                                                     DBMethodName, sayac, file_path)
 
 
-                    else:
-                        if hasattr(body, 'variable_declarators'):
-                            variableHandler(body,cName,file_path)
+                        else:
+                            if hasattr(body, 'variable_declarators'):
+                                variableHandler(body,cName,file_path)
 
-                        if hasattr(body,'block'):
-                            if hasattr(body.block, 'statements'):
-                                declarations = body.block.statements
-                                for dec in declarations:
-                                    tryBlockHandler(dec,cName,file_path,MethodDec.name)
-                                    #transactions(dec,cName,file_path)
-                        if hasattr(body,'if_true'):
-                            ifElseHandler(body,cName,file_path,MethodDec.name)
-                        if hasattr(body,'result'):
-                            returnHandler(body.result,cName,file_path,MethodDec.name)
-
+                            if hasattr(body,'block'):
+                                if hasattr(body.block, 'statements'):
+                                    declarations = body.block.statements
+                                    for dec in declarations:
+                                        tryBlockHandler(dec,cName,file_path,MethodDec.name)
+                                        #transactions(dec,cName,file_path)
+                            if hasattr(body,'if_true'):
+                                ifElseHandler(body,cName,file_path,MethodDec.name)
+                            if hasattr(body,'result'):
+                                returnHandler(body.result,cName,file_path,MethodDec.name)
+                            if hasattr(body,'body'):
+                               forHandler(body.body,cName,file_path,MethodDec.name)
             # constructro declaration
             if hasattr(MethodDec, 'block'):
                 constructorName = MethodDec.name
 
-                for block in MethodDec.block:
-                    if hasattr(block, 'rhs'):
-                        constructorMethodName = block.rhs.type.name.value
+                #for block in MethodDec.block:
+                 #   if hasattr(block, 'rhs'):
+                  #      constructorMethodName = block.rhs.type.name.value
 
 
         else:
-            methodParams = MethodDec.variable_declarators
-            for params in methodParams:
-                Insert_Variable_Invocation(cName, 'baz.java', '-', params.variable.name,
-                                           MethodDec.type.name.value,
-                                           sayac, file_path)
+            if hasattr(MethodDec,'variable_declarators'):
+                methodParams = MethodDec.variable_declarators
+                mDType=""
+                if hasattr(MethodDec.type,'name'):
+                    mDType = MethodDec.type.name.value
+                else:
+                    mDType= MethodDec.type
+                for params in methodParams:
+                    Insert_Variable_Invocation(cName, 'baz.java', '-', params.variable.name,
+                                               mDType,
+                                               sayac, file_path)
 
     except Exception as e:
         print(e, file_path)
@@ -428,20 +471,22 @@ for file in onlyfiles:
     srcfile = srczip.read(info)
     str = srcfile.decode("utf-8")
     tree = parser.parse_string(str)
-    ClassNames = tree.type_declarations
+    if(tree ==None):
+        print('Source code was broken ',file)
+    else:
+        ClassNames = tree.type_declarations
 
+        for className in ClassNames:
+            print(' ClassName: ', className.name)
 
-    for className in ClassNames:
-        print(' ClassName: ', className.name)
+            methodDecs = className.body
 
-        methodDecs = className.body
-
-        for MethodDec in methodDecs:
-            sayac=sayac+1
-            if hasattr(MethodDec, 'extends'):
-                allClasses =MethodDec.body
-                for classs in allClasses:
-                   transactions(classs,MethodDec.name,file,sayac)
-            else:
-                transactions(MethodDec,className.name,file,sayac)
+            for MethodDec in methodDecs:
+                sayac=sayac+1
+                if hasattr(MethodDec, 'extends'):
+                    allClasses =MethodDec.body
+                    for classs in allClasses:
+                       transactions(classs,MethodDec.name,file,sayac)
+                else:
+                    transactions(MethodDec,className.name,file,sayac)
 
